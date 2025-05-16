f2f8c47446cd95b0b2162f22e10215aea8961aab
Nguyễn Huỳnh Duy Tân <92073232+nixOMG@users.noreply.github.com>
2025-05-16 12:35:27 +0700

    Modify notifications to use pager

--- src/main/java/com/nix/service/NotificationService.java
+++ src/main/java/com/nix/service/NotificationService.java
@@ -1,6 +1,7 @@
 package com.nix.service;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.nix.dtos.NotificationDTO;
 import com.nix.models.User;
@@ -8,9 +9,11 @@ import com.nix.models.User;
 public interface NotificationService {
 	public void createNotification(User user, String message);
 
-	public List<NotificationDTO> getUserNotifications(User user);
+	public Page<NotificationDTO> getUserNotifications(User user, Pageable pageable);
 
-	public List<NotificationDTO> getUnreadNotifications(User user);
+	public Page<NotificationDTO> getUnreadNotifications(User user, Pageable pageable);
+	
+	public void markAllAsRead(User user);
 
 	public void markAsRead(Long notificationId, User user);
 
